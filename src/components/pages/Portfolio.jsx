13be1e4a30a676d23c51a6f898a153e86c18ce7d
J-Stiehl <salsajackie@yahoo.com>
2023-09-19 12:38:49 -0500

    added final project and first project to porfolio

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -9,6 +9,8 @@ import noteTaker from "../../assets/Screenshot-notetaker.png"
 import textEditor from "../../assets/Screenshot-pwa-texteditor.png"
 import scheduler from "../../assets/Screenshot-Work-Day-Scheduler.png"
 import sql from "../../assets/ScreenshotSQL.png"
+import minnegrowta from "../../assets/Screenshot-Minnegrowta.png"
+import sudoku from "../../assets/Screenshot-sudoku.png"
 
 export default function Portfolio() {
   return (
@@ -19,6 +21,18 @@ export default function Portfolio() {
      <div>
        
      <Row xs={1} md={2} lg={3} className="g-4">
+     {Array.from({ length: 1 }).map((_, idx) => (
+       <Col key={idx}>
+         <Card style={{ height: "60vh", backgroundColor: "#f2e9e4", objectFit: "cover"}}>
+           <Card.Title>MinneGrowta</Card.Title>
+           <Card.Img variant="top" src={minnegrowta} fluid />
+           <Card.Body>
+             <Card.Link href="https://minnegrowta-66ed74e127f3.herokuapp.com/">Deployed App</Card.Link>
+             <Card.Link href="https://github.com/laurbaur024/minnegrowta">GitHub Repo</Card.Link>
+           </Card.Body>
+         </Card>
+       </Col>
+     ))}
      {Array.from({ length: 1 }).map((_, idx) => (
        <Col key={idx}>
          <Card style={{ height: "60vh", backgroundColor: "#f2e9e4", objectFit: "cover"}}>
@@ -97,6 +111,21 @@ export default function Portfolio() {
            <Card.Body>
              <Card.Link href="https://note-taker-jstiehl-df68c54ee51b.herokuapp.com/">Deployed App</Card.Link>
              <Card.Link href="https://github.com/Jacqueline-Stiehl/express-js-note-taker-jstiehl">GitHub Repo</Card.Link>
+             <Card.Text>
+               
+             </Card.Text>
+           </Card.Body>
+         </Card>
+       </Col>
+     ))}
+     {Array.from({ length: 1 }).map((_, idx) => (
+       <Col key={idx}>
+         <Card style={{ height: "60vh", backgroundColor: "#f2e9e4", objectFit: "cover"}}>
+           <Card.Title>Sudoku</Card.Title>
+           <Card.Img variant="top" src={sudoku} />
+           <Card.Body>
+             <Card.Link href="https://ad31aid.github.io/fantastic-octo-sudoku/">Deployed App</Card.Link>
+             <Card.Link href="https://github.com/Ad31aid/fantastic-octo-sudoku">GitHub Repo</Card.Link>
              <Card.Text>
                
              </Card.Text>
